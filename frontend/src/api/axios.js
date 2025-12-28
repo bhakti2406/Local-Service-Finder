36484f20971e4eb58d05aacd472431987c0ef940
Bhakti Sudhir <bhakti.ps24@gmail.com>
2025-12-28 16:09:47 +0400

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,20 +1,21 @@
 import axios from "axios";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://local-service-finder-1-y6vz.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 api.interceptors.request.use(
   (config) => {
     const user = JSON.parse(localStorage.getItem("user"));
-
     if (user?.token) {
       config.headers.Authorization = `Bearer ${user.token}`;
     }
-
     return config;
   },
   (error) => Promise.reject(error)
 );
+
 export default api;
